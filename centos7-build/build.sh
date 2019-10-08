919778a72b7e195c13747dd7a4c42bc3b462de96
Aleksey Chudov <aleksey.chudov@gmail.com>
2019-10-08 22:31:16 +0300

    Use buildah

---
+++ centos7-build/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S bash -ex
+
+from="${REGISTRY}/centos7-systemd:${FROM_TAG}"
+name="${REGISTRY}/${IMAGE}:${SET_TAG}"
+
+# Create working container
+working_container="$(buildah from --pull "${from}")"
+
+# Customize working container
+buildah run "${working_container}" -- bash -cex '
+yum -y install "@Development Tools" vim-enhanced wget
+
+# Final cleaning
+yum clean all
+rm -frv /tmp/* /var/cache/dnf /var/log/* /var/tmp/*
+find /etc -name "*-" -o -name "*.bak" -o -name "*.rpmnew" -o -name "*.rpmsave" | xargs rm -fv
+'
+
+# Commit working container to an image
+buildah commit --format docker --rm --squash "${working_container}" "${name}"
