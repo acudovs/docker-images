ae58bcf2ded8e4c4a7c5b10bd15e81eb515d319a
Aleksey Chudov <aleksey.chudov@gmail.com>
2019-10-08 21:42:38 +0300

    Build from official CentOS 8 image

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
+# Ordered list of images to build (override with "-e" command line option)
 IMAGES = \
-	centos7-systemd \
-	centos7-build \
-	centos7-corosync-qnetd
+	centos8-systemd \
+	centos8-build
 
 REGISTRY = docker.io/alekseychudov
 FROM_TAG = latest
@@ -11,34 +11,34 @@ UMASK = 0022
 .PHONY: $(IMAGES) images pull push tag clean-all clean-build clean-containers clean-images
 
 images:
+	export REGISTRY=$(REGISTRY) FROM_TAG=$(FROM_TAG) SET_TAG=$(SET_TAG); \
 	umask $(UMASK); \
 	for image in $(IMAGES); do \
-		sed 's|$$(REGISTRY)|$(REGISTRY)|g; s|$$(FROM_TAG)|$(FROM_TAG)|g' "$${image}/Dockerfile" > "$${image}/.Dockerfile.build"; \
-		docker build --force-rm --no-cache -t "$(REGISTRY)/$${image}:$(SET_TAG)" -f "$${image}/.Dockerfile.build" "$${image}"; \
+		export IMAGE=$${image}; \
+		$${image}/build.sh; \
 	done
 
 pull:
 	for image in $(IMAGES); do \
-		docker pull "$(REGISTRY)/$${image}:$(FROM_TAG)"; \
+		podman pull "$(REGISTRY)/$${image}:$(FROM_TAG)"; \
 	done
 
 push:
 	for image in $(IMAGES); do \
-		docker push "$(REGISTRY)/$${image}:$(FROM_TAG)"; \
+		podman push "$(REGISTRY)/$${image}:$(FROM_TAG)"; \
 	done
 
 tag: pull
 	for image in $(IMAGES); do \
-		docker tag "$(REGISTRY)/$${image}:$(FROM_TAG)" "$(REGISTRY)/$${image}:$(SET_TAG)"; \
+		podman tag "$(REGISTRY)/$${image}:$(FROM_TAG)" "$(REGISTRY)/$${image}:$(SET_TAG)"; \
 	done
 
 clean-build:
-	rm -f */.Dockerfile.build
 
 clean-containers:
-	docker ps -qa | xargs docker rm --force || true
+	podman rm --all --force
 
 clean-images:
-	docker images -qa | xargs docker rmi --force || true
+	podman rmi --all --force
 
 clean-all: clean-build clean-containers clean-images
---
+++ centos8-systemd/build.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env -S bash -ex
+
+from="docker.io/library/centos:8"
+name="${REGISTRY}/${IMAGE}:${SET_TAG}"
+
+# Create working container
+working_container="$(buildah from --pull "${from}")"
+
+# Customize working container
+buildah config --cmd "/usr/lib/systemd/systemd" "${working_container}"
+buildah config --env "container=docker" "${working_container}"
+buildah config --stop-signal "SIGRTMIN+3" "${working_container}"
+
+buildah run "${working_container}" -- bash -cex '
+yum -y install epel-release
+yum -y update
+yum -y install bash-completion bind-utils iproute iputils less lsof net-tools psmisc telnet tcpdump
+
+cp /etc/skel/.bashrc /root/
+echo alias ..=\"cd ..\" >> /root/.bashrc
+echo alias l=\"ls -alF --color=auto\" >> /root/.bashrc
+
+# Setup systemd
+rm -frv /etc/systemd/system/*.wants /usr/lib/systemd/system/*.wants/*
+ln -rsvt /usr/lib/systemd/system/sockets.target.wants /usr/lib/systemd/system/dbus.socket
+ln -rsvt /usr/lib/systemd/system/sockets.target.wants /usr/lib/systemd/system/systemd-journald.socket
+ln -rsvt /usr/lib/systemd/system/sockets.target.wants /usr/lib/systemd/system/systemd-shutdownd.socket
+ln -rsvt /usr/lib/systemd/system/sysinit.target.wants /usr/lib/systemd/system/systemd-tmpfiles-setup.service
+ln -rsvt /usr/lib/systemd/system/timers.target.wants /usr/lib/systemd/system/systemd-tmpfiles-clean.timer
+
+# Do not execute umount.target to prevent shutdown errors
+sed -i "/Requires=.*umount.target/s/ *umount.target//g" /usr/lib/systemd/system/*.service
+
+# Final cleaning
+yum clean all
+rm -frv /tmp/* /var/cache/dnf /var/log/* /var/tmp/*
+find /etc -name "*-" -o -name "*.bak" -o -name "*.rpmnew" -o -name "*.rpmsave" | xargs rm -fv
+'
+
+# Commit working container to an image
+buildah commit --format docker --rm --squash "${working_container}" "${name}"
